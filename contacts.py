eac77373506c8ac856a2ac992520641c4c1134ec
bricheek <bricheek@gmail.com>
2022-12-18 06:12:45 -0500

    factory method completed

--- contacts.py
+++ contacts.py
@@ -2,12 +2,12 @@ from representations import AsDictionaryMixin
 
 class _AddressBook:
     def __init__(self):
-        self._employee_addresses= {
-            1 : Address('123 Main St', 'Dover', 'NH', '03820'),
-            2 : Address('45 Sandpiper Dr', 'Dover', 'NH', '03820', 'Apt 3'),
-            3 : Address('500 Anywhere St', 'Manchester', 'NH', '03301'),
-            4 : Address('4 Park Ave', 'New York', 'NY', '01101'),
-            5 : Address('209 Midwood Pl', 'Westfield', 'NJ', '07090'),
+        self._employee_addresses = {
+            1: Address('121 Admin Rd.', 'Concord', 'NH', '03301'),
+            2: Address('67 Paperwork Ave', 'Manchester', 'NH', '03101'),
+            3: Address('15 Rose St', 'Concord', 'NH', '03301', 'Apt. B-1'),
+            4: Address('39 Sole St.', 'Concord', 'NH', '03301'),
+            5: Address('99 Mountain Rd.', 'Concord', 'NH', '03301')
         }
 
     def get_employee_address(self, employee_id):
@@ -17,7 +17,7 @@ class _AddressBook:
         return address
 
 class Address(AsDictionaryMixin):
-    def __init__(self, street, city, state, zipcode, street2 = ''):
+    def __init__(self, street, city, state, zipcode, street2=''):
         self.street = street
         self.street2 = street2
         self.city = city
@@ -28,11 +28,11 @@ class Address(AsDictionaryMixin):
         lines = [self.street]
         if self.street2:
             lines.append(self.street2)
-        lines.append(f'{self.city}, {self.state} {self.zipcode}')
+        lines.append(f'{self.city}, {self.state}, {self.zipcode}')
         return '\n'.join(lines)
+
 
 _address_book = _AddressBook()
 
 def get_employee_address(employee_id):
     return _address_book.get_employee_address(employee_id)
-
--- program.py
+++ program.py
@@ -1,20 +1,8 @@
-
 import json
+
 from hr import calculate_payroll
 from productivity import track
 from employees import employee_database, Employee
-
-# from policy interface pattern
-# payroll_system = PayrollSystem()
-# productivity_system = ProductivitySystem()
-# employee_database = EmployeeDatabase()
-# employees = employee_database.employees_m()
-
-# manager = employees[0]
-# manager.payroll = HourlyPolicy(55)
-
-# productivity_system.track(employees, 40)
-# payroll_system.calculate_payroll(employees)
 
 def print_dict(d):
     print(json.dumps(d, indent=2))
@@ -25,5 +13,5 @@ track(employees, 40)
 calculate_payroll(employees)
 
 temp_secretary = Employee(5)
-print('Temporary Secretary: ')
+print('Temporary Secretary:')
 print_dict(temp_secretary.to_dict())
